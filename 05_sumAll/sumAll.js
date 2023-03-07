64767b8acbdd5be102746e4add3d4021607f5803
siad.elmi <siadelmi@gmail.com>
2023-03-07 18:28:45 +0200

    first two tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
-};
+function sumAll(a, b) {
+    let result = 0;
+    for (let i = a; i <= b; i++) {
+      result += i;
+    }
+    return result;
+  }
 
 // Do not edit below this line
 module.exports = sumAll;
