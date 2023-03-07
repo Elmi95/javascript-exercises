d4f591dca0e6cb7a5860b0b7cf132002e48f2bf6
siad.elmi <siadelmi@gmail.com>
2023-03-07 15:21:48 +0200

    completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+function repeatString (string,num){
+var repeatedString = '';
 
+while (num > 0){
+repeatedString+=string;
+num --;
+}
+return repeatedString;
 };
+    console.log(repeatString('hey',3))
+
 
 // Do not edit below this line
 module.exports = repeatString;
