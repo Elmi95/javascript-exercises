c8586b8577aa1a5a30606152899f23ebe3edef9f
siad.elmi <siadelmi@gmail.com>
2023-03-07 15:52:11 +0200

    completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,14 +2,5 @@ function reverseString (string){
    return string.split('').reverse().join('')
 }
 
-
-/* this reverse sentences*/
-function reverseWords(str) {
-    let reverseWordArr = str.split(" ").map(word => word.split("").reverse().join(""));
-    return reverseWordArr.join(" ");
-  }
-
-  
-
 // Do not edit below this line
 module.exports = reverseString;
